ea7d88ff9a83db6118746f4673e2c95a964c2550
Raymond Albright <67028365+razer523@users.noreply.github.com>
2021-05-11 11:10:14 -0700

    add form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
 
 export default class Contact extends Component {
     render() {
         return (
             <div>
                 <h2>Your contact information here</h2>
+                <Form>
+                    This is a contact us form to be built
+                </Form>
             </div>
         )
     }
